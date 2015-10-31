3ae2230575709e50ae472277f29d82f9a6835e9a
ldayananda <lahiru07@gmail.com>
2015-10-31 00:13:03 -0400

    Finished form controller. Still need to work out why UI is auto-updating just added form

--- public/assignment/form/form.controller.js
+++ public/assignment/form/form.controller.js
@@ -14,44 +14,45 @@
 				return null;
 			}
 
-			var form = FormService.createFormForUser(user.id, form);
-			$rootScope.forms.push(form);
-			//$scope.forms = FormService.findAllFormsForUser(user.id);
+			// Don't add a form with the extact same data
+			if ($.inArray(form, $scope.forms) == -1) {
+				FormService.createFormForUser(user.id, form);
+				$scope.forms.push(form);
+				$scope.selectedFormIndex = null;
+				$scope.selectedForm = null;
+			}
 		};
 
 		function updateForm(form) {
 			if (user == null) {
 				return null;
 			}
-			//console.log("update " + $scope.selectedFormIndex);
-			//console.log($scope.selectedForm);
-			//var form = FormService.updateFormById($scope.selectedForm.id, form);
-			//$scope.forms = FormService.findAllFormsForUser(user.id);
-			// console.log($scope.forms);
-			// console.log($scope.forms[$scope.selectedFormIndex]);
-			// console.log(form.name);
-			$scope.forms[$scope.selectedFormIndex].name = form.name;
-			// console.log("after");
-			// console.log($scope.forms);
+			var ind = $scope.selectedFormIndex;
+			var formRef = $scope.selectedForm;
+			FormService.updateFormById(formRef.id, formRef);
+			$scope.forms[ind].name = form.name;
 		}
 
 		function deleteForm(index) {
-			$scope.forms = FormService.findAllFormsForUser(user.id);
-			var allForms = $scope.forms;
+			if (user == null) {
+				return null;
+			}
+			var form = $scope.forms[index];
+			FormService.deleteFormById(form.id);
 			$scope.forms.splice(index, 1);
-			var forms = FormService.deleteFormById(allForms[index].id);
-			$rootScope.forms = forms;
 		}
 
-		function selectForm(index) {//TODO finish this
-			var allForms = $scope.forms;
-			if (allForms != null ) {
-				//console.log("ind " + index);
-				$rootScope.selectedFormIndex = index;
-				//console.log($scope.selectedFormIndex);
-				$rootScope.selectedForm = allForms[index];
-				//console.log($scope.selectedForm.name);
+		function selectForm(index) { 
+			if ($scope.forms == null ) {
+				return null;
 			}
+
+			$scope.selectedFormIndex = index;
+			$scope.selectedForm = {
+				name: $scope.forms[index].name,
+				userId: $scope.forms[index].userId,
+				id : $scope.forms[index].id
+			};
 		}
 
 		var user = $rootScope.user;
