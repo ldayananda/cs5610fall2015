c0bdb06d44a09fdd57b2b680fd357b29f9080db7
ldayananda <lahiru07@gmail.com>
2015-11-05 21:56:05 -0500

    Some last minute final touches

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cs5610fall2015
 Web Development CS 5610 Fall 2015
+
+## Project
+As of 11/5/15: The links to all pages are hardcoded into the header as controllers are not fully implemented yet. This does not follow the moqup, but this is also the only way to allow easy access to all pages. For the next assignment, the extra links will be removed.
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use('assignment', express.static(__dirname + '/public/assignment'));
 app.use('assignment/css', express.static(__dirname + '/public/assignment/css'));
-//app.use('assignment/home', express.static(__dirname + '/public/assignment/home'));
+app.use('project', express.static(__dirname + '/public/project'));
+app.use('project/css', express.static(__dirname + '/public/project/css'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
