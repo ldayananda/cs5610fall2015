666cf584091b4c9cf6247168f9072088731c95ae
ldayananda <lahiru07@gmail.com>
2015-11-23 12:53:13 -0500

    Project: Adding whitespace to pages

--- public/project/server.js
+++ public/project/server.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use('project', express.static(__dirname + '/public/project/client/views'));
+app.use('project/css', express.static(__dirname + '/public/project/css'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
